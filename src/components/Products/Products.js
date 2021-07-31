c07104ab271f5e726024b9a91e5faf8a9f7e18aa
Justin Ochoa <just.och92@gmail.com>
2021-07-31 12:11:26 -0400

    redux boiler-plate done

---
+++ src/components/Products/Products.js
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./Products.module.css";
+
+import Product from "./Product/Product";
+
+import { connect } from "react-redux";
+
+const Products = ({ products }) => {
+  return (
+    <div className={styles.products}>
+      {products.map((prod) => {
+        return <Product key={prod.id} productData={prod} />;
+      })}
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    products: state.shop.products,
+  };
+};
+
+export default connect(mapStateToProps)(Products);
